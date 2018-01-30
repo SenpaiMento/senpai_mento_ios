e527316ce6ddd0e79fdc322f17fc691c602405ae
hitsuji-haneta <hitsuji.haneta1986@gmail.com>
2018-01-30 13:14:35 +0900

    Initial Commit

---
+++ senpai_mento_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  senpai_mento_ios
+//
+//  Created by 髙橋翔太 on 2018/01/30.
+//  Copyright © 2018年 SenpaiMento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
